6824a827ab35fba28b6f271f753562637109c058
Niels Elgaard Larsen <elgaard@agol.dk>
2016-12-05 02:42:48 +0100

    now seem to work

--- restaurants/addressLookup.py
+++ restaurants/addressLookup.py
@@ -8,10 +8,10 @@ import string
 import sys
 from time import sleep
 
+limit=10 # for testing
 fixedaddrs=[]
 u8o=open(1, 'w', encoding='utf-8', closefd=False)
-fixed=open('data/fixed.jason',mode="w",encoding='utf-8')
-
+fixed=open('data/fixed.json',mode="w",encoding='utf-8')
 
 fvsterrfile='data/fvsterror.json';
 fvsterr=open(fvsterrfile,mode="r")
@@ -23,16 +23,22 @@ for adr in alist:
     alu="http://nominatim.openstreetmap.org/search?"+urlencode({"country":"Denmark","format":"json","street":adr['addr'],"postalcode":adr['postnr'],"email":"osm@agol.dk","addressdetails":0})
     print("alu= "+alu, file=u8o)
     oar=urlopen(Request(alu, headers={'User-Agent': 'Elgaard OSM Restaurant health inspection report synchronizer'}))
-    os=oar.read().decode("utf8")
+    oa=oar.read().decode("utf8")
     osm=json.loads(oa)
     print(json.dumps(osm,indent=2))
     print("#addr= "+str(len(osm)))
     if (len(osm)==1):
+        print ("got exactly one postion")
         ac=osm[0]
-        if (ac.address):
-                adr.lon=ac.lon
-                adr.lat=ac.lat
-                fixedaddrs.append(adr)
+        if (ac["osm_type"]=="node"):
+            print ("is node")
+            adr["lon"]=float(ac["lon"])
+            adr["lat"]=float(ac["lat"])
+            fixedaddrs.append(adr)
     print(" do " + alu)
     sleep(1)
+    limit=limit -1
+    if limit<0:
+        break
+    
 print(json.dumps(fixedaddrs,indent=2),file=fixed)
