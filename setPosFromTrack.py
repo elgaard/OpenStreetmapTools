657cd577e4cd3b823f23b36d4baec55071b6ea62
Niels Elgaard Larsen <elgaard@agol.dk>
2018-03-20 15:25:44 +0100

    prepare for GPS  img direction

--- setPosFromTrack.py
+++ setPosFromTrack.py
@@ -5,6 +5,7 @@ import glob
 import gzip
 import time
 from pexif import JpegFile
+from pexif import Rational
 from datetime import datetime
 
 trackf = gzip.open("track.txt.gz", 'rt')
@@ -23,28 +24,26 @@ for line in trackf.readlines():
         d=ls[3]
         tracks.append({'t':float(t),'lat':float(lat),'lon':float(lon),'heading':float(ls[3])})
 
-print("start\n")
 for fn in glob.glob("*.jpg"):
-#    print("do",fn,"\n")
-
-    ef = JpegFile.fromFile(fn)
-    primary = ef.get_exif().get_primary()
+    ef = JpegFile.fromFile(fn,mode="rw")
+    primary = ef.get_exif(create=True).get_primary()
 
     print(primary.DateTime)
-    dt=datetime.strptime(primary.DateTime,"%Y:%m:%d %H:%M:%S")
-#python 3 .timestamp()
-
+    dt=datetime.strptime(primary.DateTime,"%Y:%m:%d %H:%M:%S") #python 3 .timestamp()
     t=time.mktime(dt.timetuple())
 
     trp=None
     for tr in tracks:
         if trp is not None and t>=trp['t'] and t<=tr['t'] and tr['t']-trp['t']<20:
+            gps = primary.GPS
+
             lat=ipol(trp,tr,t,'lat')
             lon=ipol(trp,tr,t,'lon')
             h=ipol(trp,tr,t,'heading')
             print("tdiff=",tr['t']-trp['t'], " pos=",lat,",",lon, " h=",h)
             ef.set_geo(lat, lon)
-#            sign, deg, min, sec = JpegFile._parse(h)
-#            primary.GPS.GPSImgDirection=[]
+            hr=Rational(int(round(h*60)), 60)
+#            gps.GPSImgDirection=[hr] # requires patched pexif.py:     0x11: ("Image Direction", "GPSImgDirection", RATIONAL, 1),
+
             ef.writeFile(fn)
         trp=tr
